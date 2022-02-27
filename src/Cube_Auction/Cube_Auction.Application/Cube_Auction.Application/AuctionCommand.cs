1493550350da03dca0b18169541b7ff5ef803131
Luca D'Angelo <info@lucadangelo.it>
2022-02-27 11:52:07 +0000

    Ignoring ID from Auction creation contract

--- src/Cube_Auction/Cube_Auction.Application/Cube_Auction.Application/AuctionCommand.cs
+++ src/Cube_Auction/Cube_Auction.Application/Cube_Auction.Application/AuctionCommand.cs
@@ -2,11 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Cube_Auction.Application
 {
     public class AuctionCommand : IRequest<AuctionResponse>
     {
+        [JsonIgnore]
         public Guid Id { get; set; }    
         public string Name { get; set; }
 
