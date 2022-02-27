ac302dee64f67387a31bd7e0a3e91d6c353c1f9c
Luca D'Angelo <info@lucadangelo.it>
2022-02-27 22:21:38 +0000

    Updated routine for automatic creation of bids

--- src/Common/EventBusRabbitMQ/Events/BidCreationEvent.cs
+++ src/Common/EventBusRabbitMQ/Events/BidCreationEvent.cs
@@ -4,8 +4,8 @@ namespace EventBusRabbitMQ.Events
 {
     public class BidCreationEvent
     {
-        public string Id { get; set; }
-        public string AuctionName { get; set; }
+        public int IncrementalId { get; set; } //will be replaced with a GUID
+        public Guid AuctionId { get; set; }
         public double Amount { get; set; }
         public DateTime DateTime { get; set; }
         public string AuctionSubscriberName { get; set; } //this will be an ID
