a67531fd02a9ae571c7654a86d9bda6668841e56
Luca D'Angelo <info@lucadangelo.it>
2022-03-06 15:04:27 +0000

    Created Bid test project

--- src/Cube_Bid/Cube_Bid.API/BidValidator.cs
+++ src/Cube_Bid/Cube_Bid.API/BidValidator.cs
@@ -25,13 +25,21 @@ namespace Cube_Bid.API
         //NEED UT
         public int ValidateInputBid(Bid aBid)
         {
-            var auctionEventsList = _auctionsHistoryRepositoryRedis.GetAuctionsHistoriesBYAuctionIdAndEventId(aBid.AuctionId, 3); 
-            //LD 001 -> need to split the string
+            //pick the first one with value 3
+            var auctionEventsList = _auctionsHistoryRepositoryRedis.GetAuctionsHistoriesBYAuctionIdAndEventId(aBid.AuctionId, 3).First();
 
+            //LD 001 -> need to split the string in a struct. String looks like -> "03359e80-02e2-4dba-9d7d-d941e9d96056 3 06/03/2022 14:43:34 174"
+            string phrase = "The quick brown fox jumps over the lazy dog.";
+            string[] words = auctionEventsList.Split(' ');
+
+            var date = words[2];
+            var time = words[3];
+            var timeMilliseconds = words[4];
 
 
             //if both date an milliseconds
-            //if (aBid.DateTime )
+            //var something = aBid.DateTime.CompareTo((DateTime)date);
+
             //THE ABOVE METHOD will need to return a structure
             //need to do a parse of the info and then check the date
 
