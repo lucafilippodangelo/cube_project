b9a57d243c9989458946e2646d4eb8c6203884eb
Luca D'Angelo <info@lucadangelo.it>
2022-03-06 22:41:41 +0000

    Implemented UT "TestBidValidator"

--- src/Cube_Bid/BidTests/BidAsyncValidation.cs
+++ src/Cube_Bid/BidTests/BidAsyncValidation.cs
@@ -1,13 +1,94 @@
+using Cube_Bid.API;
+using Cube_Bid.API.Entities;
+using Cube_Bid.API.Repositories;
+using Moq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BidTests
 {
+    //https://www.c-sharpcorner.com/article/unit-testing-with-xunit-and-moq-in-asp-net-core/
+
+    
     public class BidAsyncValidation
     {
-        [Fact]
-        public void Test1()
+        
+
+        //MOQ
+        private readonly Mock<IAuctionsHistoryRepositoryRedis> service;
+        public BidAsyncValidation()
         {
-            Assert.Equal(4, 2+2);
+            service = new Mock<IAuctionsHistoryRepositoryRedis>();
+        }
+
+        public class TestDataGenerator : IEnumerable<object[]>
+        {
+            private readonly List<object[]> _data = new List<object[]>
+                {
+                    //Case it is NOT VALID because bid is done after auction expires
+                    new object[] {
+                        new Guid("03359e80-02e2-4dba-9d7d-d941e9d96056"),
+                        "03359e80-02e2-4dba-9d7d-d941e9d96056 3 06/03/2022 14:43:34 174",
+                        Convert.ToDateTime("06/03/2022 14:43:35"),
+                        400,
+                        2},
+
+                    //Case it is VALID because bid is done before auction expires
+                    new object[] {
+                        new Guid("03359e80-02e2-4dba-9d7d-d941e9d96056"),
+                        "03359e80-02e2-4dba-9d7d-d941e9d96056 3 06/03/2022 14:43:34 174",
+                        Convert.ToDateTime("06/03/2022 14:43:33"),
+                        400, 
+                        1},
+                    
+                    //Case it is VALID because bid is done before/"equal date" auction expires AND bidMilliseconds are <= to eventMilliseconds
+                    new object[] {
+                        new Guid("03359e80-02e2-4dba-9d7d-d941e9d96056"),
+                        "03359e80-02e2-4dba-9d7d-d941e9d96056 3 06/03/2022 14:43:34 174",
+                        Convert.ToDateTime("06/03/2022 14:43:34"),
+                        174,
+                        1},
+
+                    //Case it is NOT VALID because bid is done before/"equal date" auction expires AND bidMilliseconds are > to eventMilliseconds
+                    new object[] {
+                        new Guid("03359e80-02e2-4dba-9d7d-d941e9d96056"),
+                        "03359e80-02e2-4dba-9d7d-d941e9d96056 3 06/03/2022 14:43:34 174",
+                        Convert.ToDateTime("06/03/2022 14:43:34"),
+                        175,
+                        2}
+                };
+
+            public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void TestBidValidator(Guid eventId, string finalisedEventFromRedis, DateTime aBidDatetime, int aBidDatetimeMillisecond, int expectedResult)
+        {
+            //Arrange
+            List<string> aListOfEvents = new List<string>() { finalisedEventFromRedis };
+            
+
+            //Moq -> of a specific event "3" on that "AuctionId"
+            // so every time I call "GetAuctionsHistoriesBYAuctionIdAndEventId(aBid.AuctionId, 3)" of the mocked class "IAuctionsHistoryRepositoryRedis"
+            // "aListOfEvents" will be returned.
+            service.Setup(x => x.GetAuctionsHistoriesBYAuctionIdAndEventId(eventId, 3)).Returns(aListOfEvents);
+            BidValidator _bidValidator = new BidValidator(service.Object);
+
+
+            //Methid call
+            var resultFromValidator = _bidValidator.ValidateInputBid(new Bid() {  AuctionId = eventId,  DateTime = aBidDatetime , DateTimeMilliseconds = aBidDatetimeMillisecond});
+
+            Assert.Equal(resultFromValidator, expectedResult);
+
         }
     }
 }
+
+
+
--- src/Cube_Bid/Cube_Bid.API/BidValidator.cs
+++ src/Cube_Bid/Cube_Bid.API/BidValidator.cs
@@ -23,25 +23,44 @@ namespace Cube_Bid.API
 
 
         //NEED UT
+
         public int ValidateInputBid(Bid aBid)
         {
             //pick the first one with value 3
-            var auctionEventsList = _auctionsHistoryRepositoryRedis.GetAuctionsHistoriesBYAuctionIdAndEventId(aBid.AuctionId, 3).First();
+            var auctionEventsList = _auctionsHistoryRepositoryRedis.GetAuctionsHistoriesBYAuctionIdAndEventId(aBid.AuctionId, 3);
 
             //LD 001 -> need to split the string in a struct. String looks like -> "03359e80-02e2-4dba-9d7d-d941e9d96056 3 06/03/2022 14:43:34 174"
-            string phrase = "The quick brown fox jumps over the lazy dog.";
-            string[] words = auctionEventsList.Split(' ');
+            string[] words = auctionEventsList.First().Split(' ');
 
             var date = words[2];
             var time = words[3];
             var timeMilliseconds = words[4];
+            var dd = int.Parse(timeMilliseconds);
+
+            var parsedTime = Convert.ToDateTime(date + " " + time);
+
+            if (aBid.DateTime.CompareTo(parsedTime) > 0)
+            {
+                //so if Bid is later than "parsedTime"
+                return 2; //"two" means not valid
+            }
+            else if (aBid.DateTime.CompareTo(parsedTime) < 0)
+            {
+                return 1; //"one" means valid
+            }
+            else if (aBid.DateTimeMilliseconds > int.Parse(timeMilliseconds)) //in case it is equal then compare milliseconds
+            {
+                return 2; //"two" means not valid
+            }
+            else if (aBid.DateTimeMilliseconds <= int.Parse(timeMilliseconds)) 
+            {
+                return 1; //"one" means valid
+            }
+            else {
+                return 1; //return not valid by default
+            }
 
 
-            //if both date an milliseconds
-            //var something = aBid.DateTime.CompareTo((DateTime)date);
-
-            //THE ABOVE METHOD will need to return a structure
-            //need to do a parse of the info and then check the date
 
             return 2;
         }
