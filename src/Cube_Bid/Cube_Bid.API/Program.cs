aff2057390ab40d116cdd60b1dd0b821df530c31
Luca D'Angelo <info@lucadangelo.it>
2022-03-08 00:17:14 +0000

    code changes to set the number of max parallel thread to 1000. This allows to process bid validations threads in parallel

--- src/Cube_Bid/Cube_Bid.API/Program.cs
+++ src/Cube_Bid/Cube_Bid.API/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cube_Bid.API
@@ -13,6 +14,24 @@ namespace Cube_Bid.API
     {
         public static void Main(string[] args)
         {
+
+            //LD set the number of thread I will use to 1000
+            int minWorker, minIOC;
+            // Get the current settings.
+            ThreadPool.GetMinThreads(out minWorker, out minIOC);
+            // Change the minimum number of worker threads to four, but
+            // keep the old setting for minimum asynchronous I/O 
+            // completion threads.
+            if (ThreadPool.SetMinThreads(250, minIOC))
+            {
+                // The minimum number of threads was set successfully.
+                var t = 1;
+            }
+            else
+            {
+                // The minimum number of threads was not changed.
+                var t = 1;
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
