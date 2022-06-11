981f7a3dcbe396112383f809e21548a7e8a277c4
Luca D'Angelo <info@lucadangelo.it>
2022-06-11 23:29:42 +0100

    SignalR implemented custom message sent from server to client

--- src/Cube_BidsSignalR/Cube_BidsSignalR/Controllers/HomeController.cs
+++ src/Cube_BidsSignalR/Cube_BidsSignalR/Controllers/HomeController.cs
@@ -1,27 +1,43 @@
-﻿using Cube_BidsSignalR.Models;
+﻿using Cube_BidsSignalR.CustomSignalR;
+using Cube_BidsSignalR.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cube_BidsSignalR.Controllers
 {
     public class HomeController : Controller
     {
+        private IHubContext<AnHub> _hub;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHubContext<AnHub> hub)
         {
+            _hub = hub;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            //LD this is to test that the message is sent and visualized after page load and signalr client is connected
+            var t = Task.Run(() => ShowThreadInfo());
+            
             return View();
+            
         }
+
+        private void ShowThreadInfo()
+        {
+            Thread.Sleep(2000);
+            _hub.Clients.All.SendAsync("ReceiveMessage", "user", "message");
+        }
+
 
         public IActionResult Privacy()
         {
--- src/Cube_BidsSignalR/Cube_BidsSignalR/CustomSignalR/AnHub.cs
+++ src/Cube_BidsSignalR/Cube_BidsSignalR/CustomSignalR/AnHub.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cube_BidsSignalR.CustomSignalR
@@ -12,6 +15,26 @@ namespace Cube_BidsSignalR.CustomSignalR
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        //public async IAsyncEnumerable<int> Counter(
+        //int count,
+        //int delay,
+        //[EnumeratorCancellation]
+        //CancellationToken cancellationToken)
+        //{
+        //    for (var i = 0; i < count; i++)
+        //    {
+        //        // Check the cancellation token regularly so that the server will stop
+        //        // producing items if the client disconnects.
+        //        cancellationToken.ThrowIfCancellationRequested();
+
+        //        yield return i;
+
+        //        // Use the cancellationToken in other APIs that accept cancellation
+        //        // tokens so the cancellation can flow down to them.
+        //        await Task.Delay(delay, cancellationToken);
+        //    }
+        //}
     }
 }
 
